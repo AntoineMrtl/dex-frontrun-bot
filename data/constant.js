496e82ad46010b747ad9fe4ff25a289980877bc0
Antoine MARTEL <pro.antoine.martel@gmail.com>
2021-10-30 18:12:48 +0200

    Create constant.js

---
+++ data/constant.js
@@ -0,0 +1,37 @@
+const PANCAKE_ROUTER_ADDRESS = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const PANCAKE_FACTORY_ADDRESS = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+
+const PANCAKE_FACTORY_ABI = require("../abi/factory.js");
+const PANCAKE_POOL_ABI = require("../abi/pool.js");
+const PANCAKE_ROUTER_ABI = require("../abi/router.js");
+
+const NETWORK = "rinkeby";
+const CHAIN_SYMBOL = "ETH";
+
+
+const HTTP_PROVIDER_LINK = ``;
+const WEBSOCKET_PROVIDER_LINK = ``;
+
+
+const ETHERSCAN_API = "";
+
+const TRIGGERED_METHOD = ["swapTokensForExactTokens", "swapETHForExactTokens","swapExactTokensForTokens", "swapExactETHForTokens"];
+const GAS_LIMIT = 500000;
+
+module.exports = {
+  NETWORK,
+  CHAIN_SYMBOL,
+  PANCAKE_ROUTER_ADDRESS,
+  PANCAKE_FACTORY_ADDRESS,
+
+  PANCAKE_ROUTER_ABI,
+  PANCAKE_FACTORY_ABI,
+  PANCAKE_POOL_ABI,
+
+  HTTP_PROVIDER_LINK,
+  WEBSOCKET_PROVIDER_LINK,
+  ETHERSCAN_API,
+
+  TRIGGERED_METHOD,
+  GAS_LIMIT
+};
